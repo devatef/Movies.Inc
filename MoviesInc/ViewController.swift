2918dfb77fbb124d83140ee9bff78d7ebc091acd
atefatia <devatef@gmail.com>
2020-08-09 22:05:38 +0200

    Initial Commit

---
+++ MoviesInc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesInc
+//
+//  Created by Atef on 8/9/20.
+//  Copyright © 2020 Atef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
